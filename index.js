0ad7b4ebc0a271e2e35f56c92201e67508f4601d
tony-kobs <akr.ep17m@gmail.com>
2026-05-22 09:35:50 +0000

    Deploying to gh-pages from @ tony-kobs/sweet-deploy-team@3685cf5dedbdc8415bf639669af3d0f42c69d817 🚀

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import{a as w}from"./assets/vendor-BoNCB7p8.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const n of o.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&a(n)}).observe(document,{childList:!0,subtree:!0});function r(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function a(e){if(e.ep)return;e.ep=!0;const o=r(e);fetch(e.href,o)}})();const $="https://deserts-store.b.goit.study/api",I="/desserts",M="/categories",f=w.create({baseURL:$,headers:{"Content-Type":"application/json"}}),T=(t={})=>f.get(I,{params:t}),O=()=>f.get(M),y=document.getElementById("loader");let c=0;function h(){c+=1,y.classList.remove("hidden")}function b(){c-=1,c<=0&&(c=0,y.classList.add("hidden"))}const P="/sweet-deploy-team/assets/icons-CnkChGia.svg",p=8;let d=1,l="all",m=0;const i=document.getElementById("dessert-list"),g=document.getElementById("load-more-btn"),v=document.getElementById("category-desktop"),L=document.getElementById("category-mobile");function C(t){const s=t.map(({_id:r,image:a,category:e,name:o,description:n,price:E})=>`
-    <li class="dessert-item" data-id="${r}">
+import{a as $,i as I}from"./assets/vendor-RCRoCjUD.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))r(t);new MutationObserver(t=>{for(const o of t)if(o.type==="childList")for(const n of o.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&r(n)}).observe(document,{childList:!0,subtree:!0});function a(t){const o={};return t.integrity&&(o.integrity=t.integrity),t.referrerPolicy&&(o.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?o.credentials="include":t.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function r(t){if(t.ep)return;t.ep=!0;const o=a(t);fetch(t.href,o)}})();const T="https://deserts-store.b.goit.study/api",M="/desserts",O="/categories",f=$.create({baseURL:T,headers:{"Content-Type":"application/json"}}),P=(e={})=>f.get(M,{params:e}),C=()=>f.get(O),y=document.getElementById("loader");let i=0;function h(){i+=1,y.classList.remove("hidden")}function b(){i-=1,i<=0&&(i=0,y.classList.add("hidden"))}function v(e){I.error({title:"Помилка",message:e,position:"topRight"})}const B="/sweet-deploy-team/assets/icons-CnkChGia.svg",g=8;let c=1,d="all",m=0;const u=document.getElementById("dessert-list"),p=document.getElementById("load-more-btn"),E=document.getElementById("category-desktop"),L=document.getElementById("category-mobile");function N(e){const s=e.map(({_id:a,image:r,category:t,name:o,description:n,price:w})=>`
+    <li class="dessert-item" data-id="${a}">
       <img
-        src="${a}"
+        src="${r}"
         alt="${o}"
         class="dessert-image"
         width="303"
@@ -11,26 +11,26 @@ import{a as w}from"./assets/vendor-BoNCB7p8.js";(function(){const s=document.cre
       />
       <div class="dessert-content-wrapper">
         <div class="dessert-header-content">
-          <p class="dessert-category-name">${e.name}</p>
+          <p class="dessert-category-name">${t.name}</p>
           <h3 class="dessert-name">${o}</h3>
           <p class="dessert-description">${n}</p>
         </div>
         <div class="dessert-footer-content">
-          <p class="dessert-price">${E} грн</p>
-          <button class="dessert-modal-btn" data-id="${r}">
+          <p class="dessert-price">${w} грн</p>
+          <button class="dessert-modal-btn" data-id="${a}">
             <svg class="btn-open-modal-icon" width="24" height="24" aria-label="open full product">
-              <use href="${P}#icon-arrow_outward"></use>
+              <use href="${B}#icon-arrow_outward"></use>
             </svg>
           </button>
         </div>
       </div>
     </li>
-  `).join("");i.insertAdjacentHTML("beforeend",s)}function B(t){const s=[`<label class="category-btn">
+  `).join("");u.insertAdjacentHTML("beforeend",s)}function R(e){const s=[`<label class="category-btn">
       <input type="radio" name="category" value="all" checked hidden />
       Всі десерти
-    </label>`,...t.map(({_id:a,name:e})=>`
+    </label>`,...e.map(({_id:r,name:t})=>`
     <label class="category-btn">
-      <input type="radio" name="category" value="${a}" hidden />
-      ${e}
-    </label>`)].join("");v.innerHTML=s;const r=t.map(({_id:a,name:e})=>`<option value="${a}">${e}</option>`).join("");L.insertAdjacentHTML("beforeend",r)}async function u(t=!1){try{h(),t&&(i.innerHTML="",d=1);const{data:s}=await T({page:d,limit:p,...l!=="all"&&{category:l}});m=s.totalItems,C(s.desserts);const r=d*p;g.classList.toggle("load-more-hidden",r>=m)}catch(s){console.error("Помилка завантаження десертів:",s),i.innerHTML='<li class="error-message">Не вдалося завантажити десерти</li>'}finally{b()}}g.addEventListener("click",async()=>{d+=1,await u()});v.addEventListener("change",async t=>{t.target.name==="category"&&(l=t.target.value,await u(!0))});L.addEventListener("change",async t=>{l=t.target.value,await u(!0)});i.addEventListener("click",t=>{const s=t.target.closest(".dessert-modal-btn");if(!s)return;const r=s.dataset.id;console.log(r)});async function N(){try{h();const{data:t}=await O();B(t),await u(),g.classList.remove("load-more-hidden")}catch(t){console.error("Помилка ініціалізації:",t),i.innerHTML='<li class="error-message">Щось пішло не так</li>'}finally{b()}}N();
+      <input type="radio" name="category" value="${r}" hidden />
+      ${t}
+    </label>`)].join("");E.innerHTML=s;const a=e.map(({_id:r,name:t})=>`<option value="${r}">${t}</option>`).join("");L.insertAdjacentHTML("beforeend",a)}async function l(e=!1){try{h(),e&&(u.innerHTML="",c=1);const{data:s}=await P({page:c,limit:g,...d!=="all"&&{category:d}});m=s.totalItems,N(s.desserts);const a=c*g;p.classList.toggle("load-more-hidden",a>=m)}catch{v("Не вдалося завантажити десерти")}finally{b()}}p.addEventListener("click",async()=>{c+=1,await l()});E.addEventListener("change",async e=>{e.target.name==="category"&&(d=e.target.value,await l(!0))});L.addEventListener("change",async e=>{d=e.target.value,await l(!0)});u.addEventListener("click",e=>{const s=e.target.closest(".dessert-modal-btn");if(!s)return;const a=s.dataset.id;console.log(a)});async function S(){try{h();const{data:e}=await C();R(e),await l(),p.classList.remove("load-more-hidden")}catch{v("Щось пішло не так. Спробуйте пізніше!")}finally{b()}}S();
 //# sourceMappingURL=index.js.map
