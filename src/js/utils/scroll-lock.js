b16ffde0408673743854d0d4d64840b4cde0fc8e
Antony Kobys <akr.ep17m@gmail.com>
2026-05-30 09:12:08 +0300

    Update scroll-lock functions to use CSS variable
    Refactor scroll locking to use CSS variable for scrollbar width.

--- src/js/utils/scroll-lock.js
+++ src/js/utils/scroll-lock.js
@@ -1,17 +1,10 @@
 export function lockScroll() {
-  const scrollbarWidth =
-    window.innerWidth - document.documentElement.clientWidth;
-  document.body.style.paddingRight = `${scrollbarWidth}px`;
+  const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+  document.documentElement.style.setProperty('--scrollbar-width', `${scrollbarWidth}px`);
   document.body.style.overflow = 'hidden';
-
-  const header = document.querySelector('.header'); // ← твій клас хедера
-  if (header) header.style.paddingRight = `${scrollbarWidth}px`;
 }
 
 export function unlockScroll() {
   document.body.style.overflow = '';
-  document.body.style.paddingRight = '';
-
-  const header = document.querySelector('.header');
-  if (header) header.style.paddingRight = '';
+  document.documentElement.style.setProperty('--scrollbar-width', '0px');
 }
